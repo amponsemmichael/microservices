fb451b6014f6360a71588bc3949e0f1048ba0bb6
amponsemmichael <amponsem_michael@yahoo.com>
2024-10-21 06:50:33 +0000

    Added 2 microservices that is A and B to test my service discovery

---
+++ microservice B/microservice-B/src/main/java/com/ampons/microservice/MicroserviceBApplication.java
@@ -0,0 +1,13 @@
+package com.ampons.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBApplication.class, args);
+	}
+
+}
